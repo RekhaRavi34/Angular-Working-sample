4b2f4bac5aecc2e2ffbd8d6bb648bf325a798e2f
RekhaRavi34 <rekharavi170397@gmail.com>
2024-05-21 22:55:33 +0530

    crud with modal completed

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -1,36 +1,29 @@
 import { Injectable } from '@angular/core';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HousingService {
-    
-      private users = [
-        { id: 1, name: 'Rekha', email:"rekharavi1997@gmail.com", phone:8056224832, location:"Chennai" },
-        { id: 2, name: 'Ravi', email:"rekharavi17@gmail.com", phone:8056224832, location:"Chennai" },
-        { id: 3, name: 'loshi', email:"rekharavi17@gmail.com", phone:8056224832, location:"Chennai" },
-        { id: 4, name: 'nivi', email:"rekharavi173@gmail.com", phone:8056224832, location:"Chennai" }
-      ];
-    
-      getUsers() {
-        return this.users;
-      }
+  users: any[] = [];
 
-      deleteUser(id: number) {
-        this.users = this.users.filter(user => user.id !== id);
-      }
+  getUsers() {
+    return this.users;
+  }
 
-      addUser(user: any){
-        this.users.push(user);
-      }
-      // updateUser(updatedUser: any) {
-      //   const index = this.users.findIndex(user => user.id === updatedUser.id);
-      //   if (index !== -1) {
-      //     this.users[index] = updatedUser;
-      //   }
-      // }
+  deleteUser(id: number) {
+    this.users = this.users.filter((user) => user.id !== id);
+  }
 
-      
-      // addUser(newUser: any) {
-      //   this.users.push(newUser);
-      // }
+  addUser(user: any) {
+    this.users.push(user);
+  }
+  // updateUser(updatedUser: any) {
+  //   const index = this.users.findIndex(user => user.id === updatedUser.id);
+  //   if (index !== -1) {
+  //     this.users[index] = updatedUser;
+  //   }
+  // }
+
+  // addUser(newUser: any) {
+  //   this.users.push(newUser);
+  // }
 }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, TemplateRef, ViewChild} from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { HousingService } from '../housing.service';
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -6,92 +6,154 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
-  providers:[HousingService]
+  providers: [HousingService],
 })
-export class TableComponent implements OnInit{
-
+export class TableComponent implements OnInit {
   users: any[];
-
+  usernameTaken: boolean = false;
+  emailTaken: boolean = false;
+  formSubmitted: boolean = false;
+  modalRef: NgbModalRef | null = null;
   addUserForm: FormGroup;
-  
-  constructor(private studentservice : HousingService,private modal:NgbModal, private fb: FormBuilder){
+  isEditMode: boolean = false;
+  editedUserIndex: number | null = null;
+
+  constructor(
+    private studentservice: HousingService,
+    private modal: NgbModal,
+    private fb: FormBuilder
+  ) {
     this.addUserForm = this.fb.group({
       name: ['', Validators.required],
-      location: ['', [
-        Validators.required,
-        Validators.minLength(8),
-      ]],
+      location: ['', [Validators.required, Validators.minLength(8)]],
       email: ['', [Validators.required, Validators.email]],
-      phone: ['', [
-        Validators.required,
-        Validators.pattern('^[0-9]{10}$')
-      ]]
+      phone: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
     });
   }
 
-  get f() { return this.addUserForm.controls; }
+  get f() {
+    return this.addUserForm.controls;
+  }
 
   onSubmit() {
+    this.formSubmitted = true;
+    this.usernameTaken = false;
+    this.emailTaken = false;
+
     if (this.addUserForm.valid) {
-      this.studentservice.addUser(this.addUserForm.value);
-      this.loadUsers();
-    } else {
-      alert('Please fill out the form correctly.');
+      const newUser = this.addUserForm.value;
+      if (
+        this.isUsernameOrEmailTaken(
+          newUser.name,
+          newUser.email,
+          this.editedUserIndex
+        )
+      ) {
+        if (this.isUsernameTaken(newUser.name, this.editedUserIndex)) {
+          this.usernameTaken = true;
+        }
+        if (this.isEmailTaken(newUser.email, this.editedUserIndex)) {
+          this.emailTaken = true;
+        }
+      } else {
+        if (this.isEditMode && this.editedUserIndex !== null) {
+          this.users[this.editedUserIndex] = newUser;
+        } else {
+          this.users.push(newUser);
+        }
+        this.formSubmitted = false;
+        if (this.modalRef) {
+          this.modalRef.close();
+        }
+      }
     }
   }
 
-  
-    ngOnInit() {
-      this.loadUsers();
+  isUsernameOrEmailTaken(
+    name: string,
+    email: string,
+    editedUserIndex: number | null
+  ): boolean {
+    return (
+      this.isUsernameTaken(name, editedUserIndex) ||
+      this.isEmailTaken(email, editedUserIndex)
+    );
+  }
+
+  isUsernameTaken(name: string, editedUserIndex: number | null): boolean {
+    return this.users.some(
+      (user, index) => user.name === name && index !== editedUserIndex
+    );
+  }
+
+  isEmailTaken(email: string, editedUserIndex: number | null): boolean {
+    return this.users.some(
+      (user, index) => user.email === email && index !== editedUserIndex
+    );
+  }
+
+  ngOnInit() {
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.users = this.studentservice.getUsers();
+  }
+
+  deleteUser(user: any) {
+    this.studentservice.deleteUser(user.id);
+    this.loadUsers();
+  }
+
+  open(template: TemplateRef<any>, userIndex: number | null = null) {
+    this.isEditMode = userIndex !== null;
+    this.editedUserIndex = userIndex;
+    if (this.isEditMode && userIndex !== null) {
+      const user = this.users[userIndex];
+      this.addUserForm.patchValue(user);
+    } else {
+      this.addUserForm.reset();
     }
-  
-    loadUsers() {
-      this.users = this.studentservice.getUsers();
+    this.modalRef = this.modal.open(template);
+    // modalRef.componentInstance.name = 'World';
+  }
+
+  closeModal() {
+    if (this.modalRef) {
+      this.modalRef.close();
     }
+  }
 
-    deleteUser(user: any) {
-      this.studentservice.deleteUser(user.id);
-      this.loadUsers();
-    }
+  // for normal crud
 
-    open(template:TemplateRef<any>) {
-      this.modal.open(template);
-      // modalRef.componentInstance.name = 'World';
-    }
+  // editingUser: any = null;
+  // newUser: any = { id: null, name: '', email: '', phone: '', location:'' };
+  // showAddForm: boolean = false;
+  // editUser(user: any) {
+  //   this.editingUser = { ...user }; // Create a copy of the user object to avoid modifying the original
+  // }
 
+  // saveUser() {
+  //   this.studentservice.updateUser(this.editingUser);
+  //   this.editingUser = null; // Clear the editing user
+  // }
 
-    // for normal crud
+  // cancelEdit() {
+  //   this.editingUser = null; // Clear the editing user
+  // }
 
-    // editingUser: any = null;
-    // newUser: any = { id: null, name: '', email: '', phone: '', location:'' };
-    // showAddForm: boolean = false;
-    // editUser(user: any) {
-    //   this.editingUser = { ...user }; // Create a copy of the user object to avoid modifying the original
-    // }
-  
-    // saveUser() {
-    //   this.studentservice.updateUser(this.editingUser);
-    //   this.editingUser = null; // Clear the editing user
-    // }
-  
-    // cancelEdit() {
-    //   this.editingUser = null; // Clear the editing user
-    // }
-  
-    // toggleAddForm() {
-    //   this.showAddForm = !this.showAddForm;
-    // }
-  
-    // addUser() {
-    //   // Generate a unique ID for the new user
-    //   this.newUser.id = this.users.length + 1;
-    //   // Add the new user to the list
-    //   this.studentservice.addUser(this.newUser);
-    //   // Clear the form
-    //   this.newUser = { id: null, name: '', email: '', phone: '' };
-    //   // Reload users after addition
-    //   this.loadUsers();
-    // }
+  // toggleAddForm() {
+  //   this.showAddForm = !this.showAddForm;
+  // }
+
+  // addUser() {
+  //   // Generate a unique ID for the new user
+  //   this.newUser.id = this.users.length + 1;
+  //   // Add the new user to the list
+  //   this.studentservice.addUser(this.newUser);
+  //   // Clear the form
+  //   this.newUser = { id: null, name: '', email: '', phone: '' };
+  //   // Reload users after addition
+  //   this.loadUsers();
+  // }
 }
-
-
